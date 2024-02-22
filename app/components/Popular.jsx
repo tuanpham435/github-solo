f0b4c65e3f04a05d194ae6e2e809faaca9330586
tuan.phamvuanh <tuan.phamvuanh@educo.edu.vn>
2024-02-22 11:26:28 +0700

    6-fetch-repos

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
+import {fetchPopularRepos} from "../utils/api";
 
 function LanguageNav({selected, onUpdateLanguage}) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'PHP'];
@@ -31,28 +32,54 @@ class Popular extends Component {
         super(props);
 
         this.state = {
-            selectedLanguage: "All"
+            selectedLanguage: "All",
+            repos: null,
+            error: null
         }
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage);
+    }
+
     updateLanguage(selectedLanguage) {
         this.setState({
-            selectedLanguage
-        })
+            selectedLanguage,
+            error: null
+        });
+
+        fetchPopularRepos(selectedLanguage)
+            .then((repos) => {
+                this.setState({
+                    repos,
+                    error: null,
+                })
+            })
+            .catch((error) => {
+                console.warn("Error fetching repos:", error);
+                this.setState({
+                    error: 'There was an error fetching the repositories'
+                });
+            });
     }
 
     render() {
-        const {selectedLanguage} = this.state;
+        const {selectedLanguage, repos, error} = this.state;
 
         return (
-            <main>
-                <LanguageNav
-                    selected={selectedLanguage}
-                    onUpdateLanguage={this.updateLanguage}
-                />
-                {JSON.stringify(this.state, null, 2)}
+            <main className={'stack main-stack animate-in'}>
+                <div className={'split'}>
+                    <h1>Popular</h1>
+                    <LanguageNav
+                        selected={selectedLanguage}
+                        onUpdateLanguage={this.updateLanguage}
+                    />
+                </div>
+                {error && <p className={'text-center error'}>{error}</p>}
+
+                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
             </main>
         );
     }
